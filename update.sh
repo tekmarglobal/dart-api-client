4601460cdf01d7bd942da7e959a0573f521efa36
Yusuf TUNÇ <yusuf.tnc@gmail.com>
2020-09-27 13:45:42 +0300

    updated

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 git pull
-openapi-generator generate -i http://api.marketten.com/swagger/v1/swagger.json -g dart-dio
+mkdir swagger
+url=http://api.marketten.com/swagger/v1/swagger.json
+curl $url --output swagger/$(date +%Y%m%d-%H%M%S)-swagger.json
+openapi-generator generate -i $url -g dart-dio
 flutter pub get
 flutter packages pub run build_runner build --delete-conflicting-outputs
 sed -i ''  's|//.*||g' .gitignore
