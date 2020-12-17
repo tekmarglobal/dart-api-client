30046967d1a8f16202c8f93a316cb8038a9f028b
Emre ŞAHiN <emre@marketten.com>
2020-12-17 22:06:16 +0300

    update script'i güncellendi. Swagger.json eklendi.

--- update.sh
+++ update.sh
@@ -7,4 +7,5 @@ openapi-generator generate -i $url -g dart-dio
 flutter pub get
 flutter packages pub run build_runner build --delete-conflicting-outputs
 sed -i ''  's|//.*||g' .gitignore
+echo "DİKKAT!!! Gitte yeni eklenen dosya var mı (Unversioned file) mutlaka kontrol edin varsa git'e ekleyin. swagger.json dosyası da eklenmeli."
 
