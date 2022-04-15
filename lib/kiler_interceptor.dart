789213e6fece2eb72949a2aafa7952e5a7d35863
Ramazan Uluçay <ramazan@marketten.com>
2022-04-15 11:25:06 +0300

    nullsafety fix.

--- lib/kiler_interceptor.dart
+++ lib/kiler_interceptor.dart
@@ -1,15 +1,13 @@
 import 'package:dio/dio.dart';
 
 class KilerInterceptor extends Interceptor {
-  static Map<String, String> interceptorsMap;
+  static Map<String, String>? interceptorsMap;
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    if (interceptorsMap != null) {
-      interceptorsMap.forEach((key, value) {
-        options.headers[key] = value;
-      });
-    }
+    interceptorsMap?.forEach((key, value) {
+      options.headers[key] = value;
+    });
     super.onRequest(options, handler);
   }
 }
